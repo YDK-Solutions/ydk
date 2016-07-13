YDK-Solutions/ydk

commit bd9079867e04cf3158c65253ad76aea1de01d00e

Replace eval statements with getattr, clean up. (#160)

sdk/python/ydk/providers/_decoder.py
@@ -18,7 +18,7 @@
    Decoder.
 
 """
-
+import importlib
 from lxml import etree
 from ydk._core._dm_meta_info import ATTRIBUTE, REFERENCE_CLASS, REFERENCE_LEAFLIST, \
             REFERENCE_LIST, REFERENCE_IDENTITY_CLASS, REFERENCE_ENUM_CLASS, \
@@ -61,14 +61,12 @@ class XmlDecoder(object):
             elif contained_member.mtype == REFERENCE_LEAFLIST:
                 return XmlDecoder._to_real_list_type(rt, contained_member, entity)
             elif contained_member.mtype == REFERENCE_ENUM_CLASS:
-                exec_import = 'from ' + contained_member.pmodule_name + ' import ' + contained_member.clazz_name.split('.')[0]
-                exec exec_import
-                # first get the enum_class
-                meta_info = eval('%s._meta_info()' % contained_member.clazz_name)
+                clazz = get_class(contained_member.pmodule_name, contained_member.clazz_name)
+                meta_info = getattr(clazz, '_meta_info')()
                 enum_literal_key = rt[0].text
                 if enum_literal_key in meta_info.literal_map:
                     enum_literal = meta_info.literal_map[enum_literal_key]
-                    return eval('%s.%s' % (contained_member.clazz_name, enum_literal))
+                    return getattr(clazz, enum_literal)
             elif contained_member.mtype == REFERENCE_IDENTITY_CLASS:
                 identity_mod_name = None
                 identity_name = None
@@ -88,15 +86,9 @@ class XmlDecoder(object):
 
                 if (identity_mod_name, identity_name) in _yang_ns._identity_map:
                     (py_mod_name, identity_clazz_name) = _yang_ns._identity_map[(identity_mod_name, identity_name)]
-                    exec_import = 'from ' + py_mod_name + ' import ' + identity_clazz_name
-                    exec exec_import
-                    eval_getinstance = identity_clazz_name + '()'
-                    instance = eval(eval_getinstance)
-                    return instance
+                    return get_class_instance(py_mod_name, identity_clazz_name)
 
             elif contained_member.mtype == REFERENCE_BITS:
-                exec_import = 'from ' + contained_member.pmodule_name + ' import ' + contained_member.clazz_name.split('.')[0]
-                exec exec_import
                 keys = rt[0].text.split(" ")
                 for key in keys:
                     entity.__dict__[member.presentation_name][key] = True
@@ -212,10 +204,7 @@ class XmlDecoder(object):
             elif (member.mtype == REFERENCE_CLASS):
                 instance = entity.__dict__[member.presentation_name]
                 if instance is None:
-                    exec_import = 'from ' + member.pmodule_name + ' import ' + member.clazz_name.split('.')[0]
-                    exec exec_import
-                    eval_getinstance = member.clazz_name + '()'
-                    instance = eval(eval_getinstance)
+                    instance = get_class_instance(member.pmodule_name, member.clazz_name)
                     entity.__dict__[member.presentation_name] = instance
                     instance.parent = entity
                 XmlDecoder._bind_to_object_helper(rt[0], instance, deviation_tables, pretty_p + '-|')
@@ -224,10 +213,10 @@ class XmlDecoder(object):
                 if instance is None:
                     instance = []
                     entity.__dict__[member] = instance
-                exec_import = 'from ' + member.pmodule_name + ' import ' + member.clazz_name.split('.')[0]
-                exec exec_import
+                module = importlib.import_module(member.pmodule_name)
                 for rtchild in rt:
-                    child = eval(member.clazz_name + '()')
+                    # get nested class
+                    child = get_class_instance(member.pmodule_name, member.clazz_name)
                     child.parent = entity
                     instance.append(child)
                     XmlDecoder._bind_to_object_helper(rtchild, child, deviation_tables, pretty_p + '-l')
@@ -265,29 +254,21 @@ class XmlDecoder(object):
                     (py_mod_name, identity_clazz_name) = _yang_ns._identity_map[(identity_mod_name, identity_name)]
 
                 if py_mod_name is not None:
-                    exec_import = 'from ' + py_mod_name + ' import ' + identity_clazz_name
-                    exec exec_import
-
-                    eval_getinstance = identity_clazz_name + '()'
-                    instance = eval(eval_getinstance)
+                    instance = get_class_instance(py_mod_name, identity_clazz_name)
                     entity.__dict__[member.presentation_name] = instance
             elif member.mtype == REFERENCE_ENUM_CLASS:
                 entity.__dict__[member.presentation_name] = XmlDecoder._bind_to_enum_helper(member, rt[0])
             elif member.mtype == REFERENCE_BITS:
-                exec_import = 'from ' + member.pmodule_name + ' import ' + member.clazz_name.split('.')[0]
-                exec exec_import
                 keys = rt[0].text.split(" ")
                 for key in keys:
                     entity.__dict__[member.presentation_name][key] = True
             elif member.mtype == REFERENCE_UNION:
                 entity.__dict__[member.presentation_name] = XmlDecoder._to_real_union_type_helper(rt, member, entity)
-    
+
     @staticmethod
     def _bind_to_enum_helper(member, elem):
-        exec_import = 'from ' + member.pmodule_name + ' import ' + member.clazz_name.split('.')[0]
-        exec exec_import
-        # first get the enum_class
-        meta_info = eval('%s._meta_info()' % member.clazz_name)
+        clazz = get_class(member.pmodule_name, member.clazz_name.split('.')[0])
+        meta_info = getattr(clazz, '_meta_info')()
         enum_literal_key = elem.text
         if enum_literal_key not in meta_info.literal_map:
             sp_logger = logging.getLogger('ydk.providers.NetconfServiceProvider')
@@ -298,14 +279,24 @@ class XmlDecoder(object):
             if enum_literal_key.upper() in meta_info.literal_map:
                 sp_logger.debug('Found literal using secondary mechanism')
                 enum_literal = meta_info.literal_map[enum_literal_key.upper()]
-                return eval('%s.%s' % (member.clazz_name, enum_literal))
+                return getattr(clazz, enum_literal)
+
             elif enum_literal_key.lower() in meta_info.literal_map:
                 sp_logger.debug('Found literal using secondary mechanism')
                 enum_literal = meta_info.literal_map[enum_literal_key.lower()]
-                return eval('%s.%s' % (member.clazz_name, enum_literal))
+                return getattr(clazz, enum_literal)
         else:
             enum_literal = meta_info.literal_map[enum_literal_key]
-            return eval('%s.%s' % (member.clazz_name, enum_literal))
+            return getattr(clazz, enum_literal)
+
+
+def get_class(py_mod_name, clazz_name):
+    module = importlib.import_module(py_mod_name)
+    return reduce(getattr, clazz_name.split('.'), module)
+
+
+def get_class_instance(py_mod_name, clazz_name):
+    return get_class(py_mod_name, clazz_name)()
 
 
 def get_root(root, common_path, module_nmsp):

sdk/python/ydk/providers/_value_encoder.py
@@ -19,6 +19,7 @@
 
 """
 import logging
+import importlib
 
 from ydk._core._dm_meta_info import REFERENCE_BITS, \
     REFERENCE_IDENTITY_CLASS, REFERENCE_ENUM_CLASS, REFERENCE_LEAFLIST
@@ -29,57 +30,55 @@ import ydk.models._yang_ns as _yang_ns
 
 class ValueEncoder(object):
     def encode(self, member, NSMAP, value):
-            text = ''
-            if member.mtype == REFERENCE_IDENTITY_CLASS:
-                exec 'from %s import %s' % (member.pmodule_name, member.clazz_name.split('.')[0])
+        text = ''
+        if member.mtype == REFERENCE_IDENTITY_CLASS:
+            module = importlib.import_module(member.pmodule_name)
+            clazz = getattr(module, member.clazz_name)
 
-                if issubclass(type(value), eval(member.clazz_name)):
-                    identity_inst = value
-                    if _yang_ns._namespaces[member.module_name] == _yang_ns._namespaces[identity_inst._meta_info().module_name]:
-                        # no need for prefix in this case
-                        text = identity_inst._meta_info().yang_name
-                    else:
-                        NSMAP['idx'] = _yang_ns._namespaces[identity_inst._meta_info().module_name]
-                        text = 'idx:%s' % identity_inst._meta_info().yang_name
-
-            elif member.mtype == REFERENCE_BITS:
-                exec 'from %s import %s' % (member.pmodule_name, member.clazz_name.split('.')[0])
-
-                if isinstance(value, eval(member.clazz_name)):
-                    bits_value = value
-                    value = " ".join([k for k in bits_value._dictionary if bits_value._dictionary[k] == True])
-                    if (len(value) > 1):
-                        text = value
-            elif member.mtype == REFERENCE_ENUM_CLASS or 'Enum' in member.ptype:
-                enum_value = value
-                exec_import = 'from ' + member.pmodule_name + ' import ' + member.clazz_name.split('.')[0]
-                exec exec_import
-                enum_clazz = eval(member.clazz_name)
-                literal_map = enum_clazz._meta_info().literal_map
-                for yang_enum_name in literal_map:
-                    literal = literal_map[yang_enum_name]
-                    if enum_value == getattr(enum_clazz, literal) \
-                        or enum_value == literal:
-                        text = yang_enum_name
-                        break
-            elif member.ptype == 'bool' and isinstance(value, bool):
-                if value is True:
-                    text = 'true'
+            if issubclass(type(value), clazz):
+                identity_inst = value
+                if _yang_ns._namespaces[member.module_name] == _yang_ns._namespaces[identity_inst._meta_info().module_name]:
+                    # no need for prefix in this case
+                    text = identity_inst._meta_info().yang_name
                 else:
-                    text = 'false'
-            elif member.ptype == 'Empty' and isinstance(value, Empty):
-                pass
-            elif member.ptype == 'Decimal64' and isinstance(value, Decimal64):
-                text = value.s
-            elif member.ptype == 'str' and isinstance(value, str):
-                text = value
-            elif member.ptype == 'int' and isinstance(value, int):
-                text = str(value)
-            elif member.ptype == 'long' and isinstance(value, (int, long)):
-                text = str(value)
+                    NSMAP['idx'] = _yang_ns._namespaces[identity_inst._meta_info().module_name]
+                    text = 'idx:%s' % identity_inst._meta_info().yang_name
+        elif member.mtype == REFERENCE_BITS:
+            module = importlib.import_module(member.pmodule_name)
+            clazz = reduce(getattr, member.clazz_name.split('.'), module)
+            if isinstance(value, clazz):
+                bits_value = value
+                value = " ".join([k for k in bits_value._dictionary if bits_value._dictionary[k] == True])
+                if (len(value) > 1):
+                    text = value
+        elif member.mtype == REFERENCE_ENUM_CLASS or 'Enum' in member.ptype:
+            enum_value = value
+            module = importlib.import_module(member.pmodule_name)
+            enum_clazz = getattr(module, member.clazz_name)
+            literal_map = enum_clazz._meta_info().literal_map
+            for yang_enum_name in literal_map:
+                literal = literal_map[yang_enum_name]
+                if enum_value == getattr(enum_clazz, literal) \
+                    or enum_value == literal:
+                    text = yang_enum_name
+                    break
+        elif member.ptype == 'bool' and isinstance(value, bool):
+            if value is True:
+                text = 'true'
             else:
-                ydk_logger = logging.getLogger('ydk.providers.NetconfServiceProvider')
-                ydk_logger.info('Could not encode leaf {0}, type: {1}, {2} value: {3}'.format(member.name, member.mtype, member.ptype, value))
-
-            return text
+                text = 'false'
+        elif member.ptype == 'Empty' and isinstance(value, Empty):
+            pass
+        elif member.ptype == 'Decimal64' and isinstance(value, Decimal64):
+            text = value.s
+        elif member.ptype == 'str' and isinstance(value, str):
+            text = value
+        elif member.ptype == 'int' and isinstance(value, int):
+            text = str(value)
+        elif member.ptype == 'long' and isinstance(value, (int, long)):
+            text = str(value)
+        else:
+            ydk_logger = logging.getLogger('ydk.providers.NetconfServiceProvider')
+            ydk_logger.info('Could not encode leaf {0}, type: {1}, {2} value: {3}'.format(member.name, member.mtype, member.ptype, value))
+        return text
 

sdk/python/ydk/providers/_encoder.py
@@ -62,7 +62,7 @@ class XmlEncoder(object):
 
 
         for member in entity.i_meta.meta_info_class_members:
-            value = eval('entity.%s' % member.presentation_name)
+            value = getattr(entity, member.presentation_name)
             if value is None or isinstance(value, list) and value == []:
                 continue
 

sdk/python/ydk/providers/_provider_plugin.py
@@ -14,11 +14,11 @@
 # limitations under the License.
 # ------------------------------------------------------------------
 
-""" providers.py 
- 
+""" providers.py
+
    Service Providers module. Current implementation supports the NetconfServiceProvider which
    uses ncclient (a Netconf client library) to provide CRUD services.
-   
+
 """
 from lxml import etree
 
@@ -35,6 +35,7 @@ from ._ydk_types import _SessionTransportMode
 
 import abc
 import logging
+import importlib
 import ydk.models._yang_ns as _yang_ns
 
 
@@ -115,7 +116,7 @@ class _NCClientSPPlugin(_SPPlugin):
             for member in current._meta_info().meta_info_class_members:
                 if member.name == yang_node_name:
                     found = True
-                    current = eval('current.%s' % member.presentation_name)
+                    current = getattr(current, member.presentation_name)
                     if current is None:
                         return None
                     if isinstance(current, YList):
@@ -150,9 +151,8 @@ class _NCClientSPPlugin(_SPPlugin):
             # need to find the member that has
             top_entity_meta_info = top_entity_meta_info.parent
 
-        exec_import = 'from ' + top_entity_meta_info.pmodule_name + ' import ' + top_entity_meta_info.name.split('.')[0]
-        exec exec_import
-        entity = eval(top_entity_meta_info.name + '()')
+        module = importlib.import_module(top_entity_meta_info.pmodule_name)
+        entity = getattr(module, top_entity_meta_info.name)()
         return entity
 
     def execute_operation(self, payload, operation):
@@ -455,7 +455,7 @@ class _NCClientSPPlugin(_SPPlugin):
     def _raise_non_rpc_error(self):
         self.netconf_sp_logger.error(YPYErrorCode.INVALID_RPC)
         raise YPYServiceProviderError(YPYErrorCode.INVALID_RPC)
-    
+
     def _encode_keys(self, root, entity, meta_info):
         for key in meta_info.key_members():
             self._encode_key(root, entity, meta_info, key)
@@ -476,7 +476,7 @@ class _NCClientSPPlugin(_SPPlugin):
             if key.mtype == REFERENCE_ENUM_CLASS:
                 key_value = key_value.name.replace('_', '-').lower()
             member_elem.text = str(key_value)
-        
+
     def _get_current_tuple_list(self, current_parent, current_meta_info):
         parent_meta_tuple_list = [(current_meta_info, current_parent)]
         while hasattr(current_meta_info, 'parent'):

sdk/python/ydk/providers/_validator.py
@@ -25,7 +25,9 @@ from ydk.types import READ, DELETE, Decimal64, Empty, YList, YLeafList, YListIte
 from ydk._core._dm_meta_info import ATTRIBUTE, REFERENCE_ENUM_CLASS, REFERENCE_LIST, \
             REFERENCE_LEAFLIST, REFERENCE_IDENTITY_CLASS, REFERENCE_BITS, REFERENCE_UNION
 
+import ast
 import logging
+import importlib
 
 
 def validate_entity(entity, optype):
@@ -48,7 +50,7 @@ def validate_entity_delegate(entity, optype, errors):
         Note this method will raise ydk.errors.YPYModelError if validation fails
     """
     for member in entity.i_meta.meta_info_class_members:
-        value = eval('entity.%s' % member.presentation_name)
+        value = getattr(entity, member.presentation_name)
         if isinstance(value, READ) or isinstance(value, DELETE):
             continue
 
@@ -83,9 +85,7 @@ def _dm_validate_value(meta, value, parent, optype, errors):
 
     if meta.mtype in (REFERENCE_IDENTITY_CLASS,
         REFERENCE_BITS, REFERENCE_ENUM_CLASS, REFERENCE_LIST):
-        exec_import = 'from %s import %s' \
-            % (meta.pmodule_name, meta.clazz_name.split('.')[0])
-        exec exec_import
+        module = importlib.import_module(meta.pmodule_name)
 
     if isinstance(value, YList) and meta.mtype == REFERENCE_LIST:
         if optype == 'READ' or meta.max_elements is None:
@@ -103,10 +103,8 @@ def _dm_validate_value(meta, value, parent, optype, errors):
 
     elif meta.mtype == REFERENCE_ENUM_CLASS:
         enum_value = value
-        exec_import = 'from %s import %s' \
-            % (meta.pmodule_name, meta.clazz_name.split('.')[0])
-        exec exec_import
-        enum_clazz = eval(meta.clazz_name)
+        module = importlib.import_module(meta.pmodule_name)
+        enum_clazz = reduce(getattr, meta.clazz_name.split('.'), module)
         literal_map = enum_clazz._meta_info().literal_map
         enum_found = False
         for yang_enum_name in literal_map:
@@ -121,12 +119,13 @@ def _dm_validate_value(meta, value, parent, optype, errors):
         return value
 
     elif meta.mtype == REFERENCE_LIST:
-        if not isinstance(value, eval(meta.clazz_name)):
+        clazz = reduce(getattr, meta.clazz_name.split('.'), module)
+        if not isinstance(value, clazz):
             errmsg = "Invalid list element type, type = %s" % value
             _handle_error(meta, parent, errors, errmsg)
         return value
 
-    elif isinstance(value, eval(meta._ptype)):
+    elif value.__class__.__name__ == meta._ptype:
         if isinstance(value, (int, long)):
             return _validate_number(meta, value, parent, errors)
         elif isinstance(value, str):
@@ -163,7 +162,7 @@ def _dm_validate_value(meta, value, parent, optype, errors):
             # enum, etc.
             return value
 
-    elif isinstance(value, (int, long)) and eval(meta._ptype) in (int,long):
+    elif isinstance(value, (int, long)) and meta._ptype in ('int', 'long'):
         return _validate_number(meta, value, parent, errors)
 
     elif isinstance(value, YLeafList) and meta.mtype == REFERENCE_LEAFLIST:

sdk/python/ydk/services/crud_service.py
@@ -13,20 +13,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------
-""" services.py 
- 
-   The Services module. 
-   
+""" services.py
+
+   The Services module.
+
    Supported Services Include
-   
-     CRUDService: Provide Create/Read/Update/Delete API's 
-     
+
+     CRUDService: Provide Create/Read/Update/Delete API's
+
 """
 from ydk.errors import YPYServiceError
 from ydk.types import YList
 from .service import Service
 from meta_service import MetaService
 import logging
+import importlib
 
 
 class CRUDService(Service):
@@ -35,49 +36,49 @@ class CRUDService(Service):
         self.service_logger = logging.getLogger(__name__)
 
     def create(self, provider, entity):
-        """ Create the entity 
-        
+        """ Create the entity
+
             Args:
                 provider: An instance of ydk.providers.ServiceProvider
                 entity: An instance of an entity class defined under the ydk.models package or subpackages
 
            Returns:
                  None
-        
+
            Raises:
               `YPYModelError <ydk.errors.html#ydk.errors.YPYModelError>`_ if validation.
-              `YPYServiceError <ydk.errors.html#ydk.errors.YPYServiceError>`_ if other error has occurred. Possible errors could be 
+              `YPYServiceError <ydk.errors.html#ydk.errors.YPYServiceError>`_ if other error has occurred. Possible errors could be
                   - a server side error
                   - if there isn't enough information in the entity to prepare the message (missing keys for example)
-                  
-                  
+
+
         """
         MetaService.normalize_meta(provider._get_capabilities(), entity)
         self._execute_crud_operation_on_provider(provider, entity, 'CREATE', False)
 
     def delete(self, provider, entity):
-        """ Delete the entity 
-        
+        """ Delete the entity
+
             Args:
                 provider: An instance of ydk.providers.ServiceProvider
                 entity: An an entity class defined under the ydk.models package or subpackages
-                
-           
+
+
 
            Returns:
                  None
-        
+
            Raises:
-              `YPYModelError <ydk.errors.html#ydk.errors.YPYModelError>`_ if validation failed. 
+              `YPYModelError <ydk.errors.html#ydk.errors.YPYModelError>`_ if validation failed.
               `YPYServiceError <ydk.errors.html#ydk.errors.YPYServiceError>`_ if other error has occurred. Possible errors could be a service side error
               or if there isn't enough information in the entity to prepare the message (missing keys for example)
-                  
+
         """
         MetaService.normalize_meta(provider._get_capabilities(), entity)
         self._execute_crud_operation_on_provider(provider, entity, 'DELETE', False)
 
     def update(self, provider, entity):
-        """ Update the entity 
+        """ Update the entity
 
             Args:
                - provider: An instance of ydk.providers.ServiceProvider
@@ -94,7 +95,7 @@ class CRUDService(Service):
               `YPYModelError <ydk.errors.html#ydk.errors.YPYModelError>`_ if validation failed.
               `YPYServiceError <ydk.errors.html#ydk.errors.YPYServiceError>`_ if other error has occurred. Possible errors could be a service side error
               or if there isn't enough information in the entity to prepare the message (missing keys for example)
-                  
+
         """
         # first read the object
         if self._entity_exists(provider, entity):
@@ -104,28 +105,28 @@ class CRUDService(Service):
 
     def read(self, provider, read_filter, only_config=False):
         """ Read the entity or entities
-            
+
             Args:
                 - provider: An instance of ydk.providers.ServiceProvider
                 - read_filter: A read_filter is an instance of an entity class.
-                
+
                          An entity class is a class defined under the ydk.models package that is not an Enum , Identity of subclass of FixedBitsDict) .
                          Attributes of this entity class may contain values that act as match expressions or can be explicitly marked as to be read by assigning an instance of ydk.types.READ class to them.
-                
+
                 - only_config: Flag that indicates that only data that represents configuration data is to be fetched.
-                        
+
                             Default is set to False i.e. both oper and config data will be fetched.
-                
+
            Returns:
                  The entity or entities as identified by the read_filter.
-        
+
            Raises:
               `YPYModelError <ydk.errors.html#ydk.errors.YPYModelError>`_ if validation failed.
               `YPYServiceError <ydk.errors.html#ydk.errors.YPYServiceError>`_ if other error has occurred. Possible errors could be
                   - a server side error
                   - if there isn't enough information in the entity to prepare the message (missing keys for example)
                   - if the type to be returned cannot be determined.
-                  
+
         """
         MetaService.normalize_meta(provider._get_capabilities(), read_filter)
         self._perform_read_filter_check(read_filter)
@@ -167,13 +168,12 @@ class CRUDService(Service):
         return True
 
     def _create_update_entity_filter(self, entity):
-        stmt = 'from ' + entity._meta_info().pmodule_name + ' import ' + entity._meta_info().name.split('.')[0]
-        exec(stmt)
+        module = importlib.import_module(entity._meta_info().pmodule_name)
+        update_filter = getattr(module, entity._meta_info().name)()
 
-        update_filter = eval('%s()' % entity._meta_info().name)
         # copy over the keys
         for key_member in entity._meta_info().key_members():
-            key_val = eval('entity.%s' % key_member.presentation_name)
+            key_val = getattr(entity, key_member.presentation_name)
             update_filter.__dict__[key_member.presentation_name] = key_val
 
         return update_filter

commit 7702571ca221a4dcc3bd1c7573a84135d87a94bb

Fixed bug introduced in (#160), replaced eval statements in (#174).

sdk/python/ydk/providers/_decoder.py
@@ -267,10 +267,8 @@ class XmlDecoder(object):
 
     @staticmethod
     def _bind_to_enum_helper(member, elem):
-        exec_import = 'from ' + member.pmodule_name + ' import ' + member.clazz_name.split('.')[0]
-        exec exec_import
-        # first get the enum_class
-        meta_info = eval('%s._meta_info()' % member.clazz_name)
+        enum_clazz = get_class(member.pmodule_name, member.clazz_name)
+        meta_info = enum_clazz._meta_info()
         enum_literal_key = elem.text
         if enum_literal_key not in meta_info.literal_map:
             sp_logger = logging.getLogger('ydk.providers.NetconfServiceProvider')
@@ -281,15 +279,15 @@ class XmlDecoder(object):
             if enum_literal_key.upper() in meta_info.literal_map:
                 sp_logger.debug('Found literal using secondary mechanism')
                 enum_literal = meta_info.literal_map[enum_literal_key.upper()]
-                return eval('%s.%s' % (member.clazz_name, enum_literal))
+                return getattr(enum_clazz, enum_literal)
 
             elif enum_literal_key.lower() in meta_info.literal_map:
                 sp_logger.debug('Found literal using secondary mechanism')
                 enum_literal = meta_info.literal_map[enum_literal_key.lower()]
-                return eval('%s.%s' % (member.clazz_name, enum_literal))
+                return getattr(enum_clazz, enum_literal)
         else:
             enum_literal = meta_info.literal_map[enum_literal_key]
-            return eval('%s.%s' % (member.clazz_name, enum_literal))
+            return getattr(enum_clazz, enum_literal)
 
 
 def get_class(py_mod_name, clazz_name):
@@ -394,4 +392,3 @@ def is_digit(n):
         return True
     except ValueError:
         return  False
-

sdk/python/ydk/providers/_value_encoder.py
@@ -33,7 +33,7 @@ class ValueEncoder(object):
         text = ''
         if member.mtype == REFERENCE_IDENTITY_CLASS:
             module = importlib.import_module(member.pmodule_name)
-            clazz = getattr(module, member.clazz_name)
+            clazz = reduce(getattr, member.clazz_name.split('.'), module)
 
             if issubclass(type(value), clazz):
                 identity_inst = value
@@ -53,9 +53,8 @@ class ValueEncoder(object):
                     text = value
         elif member.mtype == REFERENCE_ENUM_CLASS or 'Enum' in member.ptype:
             enum_value = value
-            exec_import = 'from ' + member.pmodule_name + ' import ' + member.clazz_name.split('.')[0]
-            exec exec_import
-            enum_clazz = eval(member.clazz_name)
+            module = importlib.import_module(member.pmodule_name)
+            enum_clazz = reduce(getattr, member.clazz_name.split('.'), module)
             literal_map = enum_clazz._meta_info().literal_map
             for yang_enum_name in literal_map:
                 literal = literal_map[yang_enum_name]
@@ -82,4 +81,3 @@ class ValueEncoder(object):
             ydk_logger = logging.getLogger('ydk.providers.NetconfServiceProvider')
             ydk_logger.info('Could not encode leaf {0}, type: {1}, {2} value: {3}'.format(member.name, member.mtype, member.ptype, value))
         return text
-
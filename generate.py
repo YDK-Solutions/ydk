d49b5803030f00d69dcea3b473ac190b3c0de93e
Tim Thomas <tithomas@cisco.com>
2017-04-14 14:58:59 -0400

    Correct message handling in various exception raises (#440)
    * Correct message handling in various exception raises
    
    * Catch YdkGenException to avoid traceback in main

--- generate.py
+++ generate.py
@@ -31,6 +31,7 @@ import re
 
 from git import Repo
 from ydkgen import YdkGenerator
+from ydkgen.common import YdkGenException
 
 
 logger = logging.getLogger('ydkgen')
@@ -288,23 +289,29 @@ if __name__ == '__main__':
     elif options.python:
         language = 'python'
 
-    if options.bundle:
-        output_directory = (YdkGenerator(
-                            output_directory,
-                            ydk_root,
-                            options.groupings_as_class,
-                            options.gentests,
-                            language,
-                            'bundle').generate(options.bundle))
+    try:
+        if options.bundle:
+            output_directory = (YdkGenerator(
+                                output_directory,
+                                ydk_root,
+                                options.groupings_as_class,
+                                options.gentests,
+                                language,
+                                'bundle').generate(options.bundle))
 
-    if options.core:
-        output_directory = (YdkGenerator(
-                            output_directory,
-                            ydk_root,
-                            options.groupings_as_class,
-                            options.gentests,
-                            language,
-                            'core').generate(options.core))
+        if options.core:
+            output_directory = (YdkGenerator(
+                                output_directory,
+                                ydk_root,
+                                options.groupings_as_class,
+                                options.gentests,
+                                language,
+                                'core').generate(options.core))
+    except YdkGenException as e:
+        print('Error(s) occurred in YdkGenerator()!')
+        if not options.verbose:
+            print(e.msg)
+        sys.exit(1)
 
     if options.gendoc:
         generate_documentations(output_directory, ydk_root, language, options.bundle, options.core)

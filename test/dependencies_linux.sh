3f7b6052688c5ac169768867a506aa8d3294a077
shgandhi <shruti4gandhi@gmail.com>
2017-07-19 11:25:40 -0700

    Fix typos in dependencies

--- test/dependencies_linux.sh
+++ test/dependencies_linux.sh
@@ -104,7 +104,7 @@ function install_grpc {
     cd grpc
     git submodule update --init
     make
-    [sudo] make install
+    sudo make install
 }
 ########################## EXECUTION STARTS HERE #############################
 

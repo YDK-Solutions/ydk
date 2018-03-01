cb7c14c91792e78fdda2533ef56ca7f60b8c2b14
Abhi Keshav <abhirame@cisco.com>
2018-03-01 13:01:43 -0500

    Split deps (#701)

--- test/dependencies_centos_basic.sh
+++ test/dependencies_centos_basic.sh
@@ -26,37 +26,31 @@ function print_msg {
     echo -e "${RED}*** $(date) *** dependencies_centos.sh | $1${NOCOLOR}"
 }
 
+function install_dependencies {
+    print_msg "Installing dependencies"
 
-function install_confd {
-    print_msg "Installing confd"
+    yum update -y > /dev/null
+    yum install epel-release -y > /dev/null
+    yum install https://centos7.iuscommunity.org/ius-release.rpm -y > /dev/null
+    yum install git which libxml2-devel libxslt-devel libssh-devel libtool gcc-c++ pcre-devel \
+                cmake3 wget curl-devel unzip python-devel python-pip make go java sudo \
+                python36u-devel python36u-pip -y  \
+                > /dev/null
 
-    wget https://github.com/CiscoDevNet/ydk-gen/files/562538/confd-basic-6.2.linux.x86_64.zip
-    unzip confd-basic-6.2.linux.x86_64.zip
-    ./confd-basic-6.2.linux.x86_64.installer.bin ../confd
+     print_msg "Installing gcc5"
+     yum install centos-release-scl -y > /dev/null
+     yum install devtoolset-4-gcc* -y > /dev/null
+
+     ln -sf /opt/rh/devtoolset-4/root/usr/bin/gcc /usr/bin/cc
+     ln -sf /opt/rh/devtoolset-4/root/usr/bin/g++ /usr/bin/c++
+
+     which gcc
+     gcc --version
+     print_msg "Done installing gcc5"
 }
 
-function install_openssl {
-    print_msg "Installing openssl 0.1.0u for confd"
 
-    wget https://www.openssl.org/source/openssl-1.0.1u.tar.gz
-    tar -xvzf openssl-1.0.1u.tar.gz > /dev/null
-    cd openssl-1.0.1u
-    ./config shared  > /dev/null && make all > /dev/null
-    cp libcrypto.so.1.0.0 ../../confd/lib
-    cd -
-
-    print_msg "Done Installing openssl 0.1.0u"
-}
-
-function install_fpm {
-    print_msg "Installing fpm"
-    yum install ruby-devel gcc make rpm-build rubygems -y > /dev/null
-    gem install --no-ri --no-rdoc fpm
-}
 
 ########################## EXECUTION STARTS HERE #############################
 
-./test/dependencies_centos_basic.sh
-install_confd
-install_openssl
-install_fpm
+install_dependencies

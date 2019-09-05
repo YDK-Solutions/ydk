69b1e681c1e689be8dea6bac4d09b648c892c429
ygorelik <ygorelik@cisco.com>
2019-09-05 14:18:18 -0700

    Fixing Travis tests for Ubuntu

--- test/dependencies_ubuntu.sh
+++ test/dependencies_ubuntu.sh
@@ -28,9 +28,9 @@ function install_dependencies {
     print_msg "Installing dependencies"
 
     # apt-get update -y > /dev/null
-    apt-get install sudo -y > /dev/null
+    apt install sudo -y > /dev/null
     sudo apt-get install -y --no-install-recommends apt-utils
-    # sudo apt-get update -y > /dev/null
+    sudo apt-get update -y > /dev/null
     sudo apt-get install libtool-bin -y > /dev/null
     local status=$?
     if [[ ${status} != 0 ]]; then

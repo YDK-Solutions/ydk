cddf6e26048185086640b419ea97a097e9a67e86
ygorelik <yangorelik@yahoo.com>
2020-07-20 11:17:18 -0700

    Lowered version of go testify version

--- test/gnmi_tests.sh
+++ test/gnmi_tests.sh
@@ -169,7 +169,10 @@ function init_go_env {
     go_version=$(echo `go version` | awk '{ print $3 }' | cut -d 'o' -f 2)
     print_msg "Current Go version is $go_version"
 
-    go get github.com/stretchr/testify -b v1.6.1
+    go get github.com/stretchr/testify
+    cd $GOPATH/src/github.com/stretchr/testify
+    git checkout tags/v1.6.1
+    cd -
 
     export CGO_ENABLED=1
     export CGO_LDFLAGS_ALLOW="-fprofile-arcs|-ftest-coverage|--coverage"
